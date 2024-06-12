64b7c78f5c677cc92683f5143066fc30228b5a2c
smuc06 <164278565+smuc06@users.noreply.github.com>
2024-06-12 11:10:23 +0900

    Mypage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    setIsLoggedIn(false);
+    router.push("/login");
+  };
+
   return (
     <header className="bg-white shadow-md py-4 px-8 fixed w-full z-40">
       <div className="container mx-auto flex justify-between items-center">
@@ -10,10 +29,23 @@ export default function Navbar() {
             메인화면으로
           </p>
         </Link>
-        <nav>
-          <Link href="/mypage">
-            <p className="px-4 cursor-pointer">마이페이지</p>
-          </Link>
+        <nav className="flex space-x-4">
+          {isLoggedIn ? (
+            <>
+              {router.pathname !== "/mypage" && (
+                <Link href="/mypage">
+                  <p className="cursor-pointer">마이페이지</p>
+                </Link>
+              )}
+              <p className="cursor-pointer" onClick={handleLogout}>
+                로그아웃
+              </p>
+            </>
+          ) : (
+            <Link href="/login">
+              <p className="cursor-pointer">로그인</p>
+            </Link>
+          )}
         </nav>
       </div>
     </header>
