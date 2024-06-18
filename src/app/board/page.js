2e6f720f9ed07f8bc091a96a004c93788c907762
Mingyu Kim <mlnudg2000@gmail.com>
2024-06-18 17:12:15 +0900

    Feat: Mainpage and boards

---
+++ src/app/board/page.js
@@ -0,0 +1,10 @@
+export default function Board() {
+  return (
+    <main className="container mx-auto px-4 pt-16">
+      <div className="text-center my-8">
+        <h1 className="text-3xl font-bold mb-4">게시판 페이지</h1>
+        <p className="text-lg text-gray-700">여기는 게시판 페이지입니다.</p>
+      </div>
+    </main>
+  );
+}
